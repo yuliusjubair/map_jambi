a1bc7b610edba67f403ffb9d467952790123570b
julius <monox.narsis@gmail.com>
2021-01-02 15:39:57 +0700

    db config

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,8 @@ class Home extends CI_Controller
 {
 	 function __construct() {
         parent::__construct();
-        $this->load->model('Map_model');
+        
+        //$this->load->model('Map_model');
     }
 
     public function index()
